d3c5131f82506b83248bf8d3906224774c502bd3
tcodespace <xmengspace@qq.com>
2025-09-04 15:52:59 +0800

    ✨ feat:  文件目录查看

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { join } from "path";
 import express from "express";
+import serveIndex from "serve-index";
 import { getLocalIPv4 } from "./utils/index.js";
 
 const app = express();
 
-app.use("/release", express.static(join(import.meta.dirname, "../packages")));
+const packagesPath = join(import.meta.dirname, "../packages");
+
+app.use("/release", express.static(packagesPath), serveIndex(packagesPath));
 
 app.listen(3000, () => {
   const ip = getLocalIPv4();
